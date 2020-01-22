a0eba3e6c002173dd02875390ff03dcec68dff81
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-01-22 22:23:13 +0900

    feat(404): add 404 and redirect for github pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'school-scout';
+  constructor(private router: Router) {
+    const path = localStorage.getItem('path');
+    if (path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+  }
 }
