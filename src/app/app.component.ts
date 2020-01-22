912bd466b25caac12d5f78d5ffc2dc3bcc731ee9
Daichi Ninomiya <daichi.ninomiya@deer.co.jp>
2020-01-22 23:01:36 +0900

    chore(deploy): gh-pages to firebase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,11 +6,4 @@ import { Router } from '@angular/router';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  constructor(private router: Router) {
-    const path = localStorage.getItem('path');
-    if (path) {
-      localStorage.removeItem('path');
-      this.router.navigate([path]);
-    }
-  }
 }
